b6d9a2a29d33e2a4c7acce8c091a08a67cefa028
Abdi <sulistyawanar@gmail.com>
2024-06-03 23:53:37 +0800

    initial commit

---
+++ database/migrations/2024_06_03_060202_create_patient_appointments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patient_appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_id')->constrained('patients')->cascadeOnDelete();
+            $table->foreignId('doctor_id')->constrained('users')->cascadeOnDelete();
+            $table->date('date_of_appointment');
+            $table->time('time');
+            $table->string('note')->nullable();
+            $table->string('prescription')->nullable();
+            $table->string('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patient_appointments');
+    }
+};
